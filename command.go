233cd9364fde24b145a6b9ff6ccac7917954d954
wreulicke <masaya2dies@gmail.com>
2021-05-03 01:25:08 +0900

    simplify

--- command.go
+++ command.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"os"
 	"os/exec"
 	"sync"
@@ -14,44 +13,15 @@ type Command struct {
 func NewCommand(p Profile, args []string) *Command {
 	c := exec.Command(args[0], args[1:]...)
 	c.Env = append(os.Environ(), "AWS_PROFILE="+p.Name)
-	return &Command{c}
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stdout
+	return &Command{Cmd: c}
 }
 
 func (c *Command) Exec(end <-chan struct{}, ch chan<- string, group *sync.WaitGroup) error {
-	stdout, err := c.StdoutPipe()
-	if err != nil {
-		return err
-	}
-	stderr, err := c.StderrPipe()
-	if err != nil {
-		return err
-	}
-
 	if err := c.Start(); err != nil {
-		stdout.Close()
-		stderr.Close()
 		return err
 	}
 
-	group.Add(2)
-	scout := bufio.NewScanner(stdout)
-	go func() {
-		for scout.Scan() {
-			ch <- scout.Text()
-		}
-		stdout.Close()
-		group.Done()
-		<-end
-	}()
-
-	scerr := bufio.NewScanner(stderr)
-	go func() {
-		for scerr.Scan() {
-			ch <- scerr.Text()
-		}
-		stderr.Close()
-		group.Done()
-	}()
-
-	return nil
+	return c.Wait()
 }
